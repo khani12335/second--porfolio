45c3b048e5095c044606dbddeea55ddbbe3d10c5
dani_khan <159428334+khani12335@users.noreply.github.com>
2024-11-10 05:14:47 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,100 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen relative">
+      <div
+        className="flex items-center w-full h-full bg-cover bg-center overflow-scroll"
+        style={{ backgroundImage: "url(/main-bg.webp)" }}
+      >
+        <div className="pl-10 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px] ">
+          <h1 className="text-[30px] md:text-[50px] text-white font-semibold">
+            Make Anything possible with
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+              {" "}
+              Web development
+            </span>
+          </h1>
+          <p className="text-gray-300 hidden md:block">
+            Welcome to my portfolio! I am a passionate frontend developer
+            dedicated to building responsive and visually stunning websites.
+            Explore my work and skills, and lets create something amazing
+            together.
+          </p>
+          <div className="flex-col md:flex-row   md:flex gap-5">
+            <Link
+              href="/skills"
+              className="rounded-[20px] relative bg-blue-500 hover:bg-blue-400  px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              Learn more
+            </Link>
+            <Link
+              href="/projects"
+              className="rounded-[20px] hover:bg-blue-400  px-5 py-3 text-lg text-white max-w-[200px] border"
+            >
+              My Projects
+            </Link>
+            <Link
+              href="/contact"
+              className="rounded-[20px] hover:bg-blue-400  px-5 py-3 text-lg text-white max-w-[200px] border"
+            >
+              Contact Me
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex absolute bottom-20 z-[20] right-4 flex-col md:hidden gap-4">
+        <Link
+          href="/skills"
+          className="rounded-[20px] bg-blue-500 hover:bg-blue-400  px-[10px] text-center py-2 text-md text-white w-[120px]"
+        >
+          Learn more
+        </Link>
+        <Link
+          href="/projects"
+          className="rounded-[20px] bg-blue-500 hover:bg-blue-400  px-[10px] text-center py-2 text-md text-white w-[120px]"
+        >
+          My Projects
+        </Link>
+        <Link
+          href="/contact"
+          className="rounded-[20px] bg-blue-500 hover:bg-blue-400  px-[10px] text-center py-2 text-md text-white w-[120px]"
+        >
+          Contact Me
+        </Link>
+      </div>
+
+      <div className="absolute bottom-0 right-0 z-[10]">
+        <Image
+          src="/horse.png"
+          alt="horse"
+          width={300}
+          height={300}
+          className="absolute right-55 top-40"
+        />
+
+        <Image src="/cliff.webp" alt="cliff" height={480} width={480} />
+      </div>
+
+      <div className="absolute w-full h-auto bottom-0 z-[5]">
+        <Image
+          src="/trees.webp"
+          alt="trees"
+          height={2000}
+          width={2000}
+          className="h-full w-full"
+        />
+      </div>
+
+      <Image
+        src="/stars.png"
+        alt="horse"
+        width={300}
+        height={300}
+        className="absolute left-0 top-10 z-[10]"
+      />
+    </main>
+  );
+}
---
+++ src/constants/index.tsx
@@ -0,0 +1,127 @@
+
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Linkedin",
+    src: "/icons8-linkedin.svg",
+    href: "https://www.linkedin.com/in/mr-danish-1b954a2b6/",
+  },
+  {
+    name: "GitHub",
+    src: "/icons8-github.svg",
+    href: "https://github.com/khani12335",
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+    href: "",
+  },
+];
+export const Projects = [
+  {
+    title: "crypto exchnage",
+    text: "I developed a full-stack blog web application with a user-friendly interface. Built using React.js and Tailwind CSS,The backend, powered by Appwrite",
+    src: "/crypto.PNG",
+    Link:"https://vercel.com/danishs-projects-0d90b476/crypto-k7cq/Cjpy2NcNWtbRFevuBfMBW98CdQEw"
+  },
+  {
+    title: "Digital clock",
+    text: "Created a responsive and visually engaging personal portfolio website using React Js ,Tailwind CSS and GSAP. This project highlights my journey in web development",
+    src: "/clock.PNG",
+    Link:"https://vercel.com/danishs-projects-0d90b476/watch/9VSAcsWFmZ9QxHWFxDvzgWDtiky6"
+  },
+  {
+    title: "reume builder",
+    text: "Built a responsive To-Do List app with React, Context API, and Tailwind CSS, featuring task management with add, edit, and delete functions for a seamless user experience. Hosted on Netlify.",
+    Link:"https://vercel.com/danishs-projects-0d90b476/milestone-04/8JNZQeYyEQXG4QmREkpcq9xWxmEE",
+    src: "/resume.PNG"
+  },
+  {
+    title: "cloth Search website",
+    text: "This app allows users to search for recipes by simply entering an ingredient. It pulls recipe information like the title, and instructions using the Edamam API.",
+    src: "/cloth.PNG",
+    Link:"https://www.limelight.pk/"
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/skills",
+    icon: RxPerson,
+    link: "/skills",
+  },
+  {
+    name: "/projects",
+    icon: RxDashboard,
+    link: "/projects",
+  },
+  {
+    name: "/contact",
+    icon: RxClipboard,
+    link: "/contact",
+  },
+];
